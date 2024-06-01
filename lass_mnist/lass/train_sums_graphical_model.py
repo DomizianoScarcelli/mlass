c2c8a508201f9b7f5a8f5c46fe4f82328fba5651
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2024-06-01 20:07:31 +0200

    added train sums for graphical model solution

--- lass_mnist/lass/train_sums_graphical_model.py
+++ lass_mnist/lass/train_sums_graphical_model.py
@@ -10,31 +10,64 @@ from torch.utils.tensorboard import SummaryWriter
 from torchvision.utils import make_grid
 from .utils import CONFIG_DIR, CONFIG_STORE, ROOT_DIR
 
+NUM_SOURCES = 3
 
 def roll(x, n):
     return torch.cat((x[:, -n:], x[:, :-n]), dim=1)
 
+def split_images_by_step(images: torch.Tensor, batch_size: int, step: int) -> torch.Tensor:
+    image_batches = []
+    batched_images_size = batch_size // step
+    for i in range(len(images) // batched_images_size):
+        image_batches.append(
+            images[i * batched_images_size: (i + 1) * batched_images_size])
+    return torch.stack(image_batches)
+
+def get_mixtures(images: torch.Tensor) -> torch.Tensor:
+    mixtures = []
+    for i in range(len(images)):
+        # Build the mixtures of the images up to index i
+        mixture = torch.mean(images[:i], dim=0)
+        mixtures.append(mixture)
+    return torch.stack(mixtures)
+
 
 def train(data_loader, sums, model, args, writer, step):
-    for images, _ in data_loader:
+    for images, _ in tqdm(data_loader, desc="Training sums"):
         images = images.to(args.device)
-        images1 = images[: args.batch_size // 2]
-        images2 = images[args.batch_size // 2 :]
-        images_mixture = 0.5 * images1 + 0.5 * images2
+        # Shape: torch.Size([3, 21, 1, 28, 28] for NUM_SOURCES=3
+        source_images = split_images_by_step(images, args.batch_size, step=NUM_SOURCES)
+        # List of n tensors, each one is $m_i$, the mixture at step i, meaning
+        # the mixture of the images 0,1,...,i-1
+        # Shape: torch.Size([3, 21, 1, 28, 28] for NUM_SOURCES=3
+        images_mixtures = get_mixtures(images=source_images)
 
         with torch.no_grad():
-            _, z_e_x1, _ = model(images1)
-            _, z_e_x2, _ = model(images2)
-            _, z_e_x_mixture, _ = model(images_mixture)
-            codes1 = model.codeBook(z_e_x1)
-            codes2 = model.codeBook(z_e_x2)
-            codes_mixture = model.codeBook(z_e_x_mixture)
+            z_e_xs = [model(images)[1] for images in source_images]
+            z_e_mixtures = [model(image_mixture)[1] for image_mixture in images_mixtures]
+            # z_e_xs = []
+            # z_e_mixtures = []
+            # for images in source_images:
+            #     _, z_e_x, _ = model(images)
+            #     z_e_xs.append(z_e_x)
 
-        codes1 = codes1.flatten()
-        codes2 = codes2.flatten()
-        codes_mixture = codes_mixture.flatten()
+            # for image_mixture in images_mixtures:
+            #     _, z_e_mixture, _ = model(image_mixture)
+            #     z_e_mixtures.append(z_e_mixture)
+            
+        codes = [model.codeBook(z_e_x) for z_e_x in z_e_xs]
+        codes_mixtures = [model.codeBook(z_e_x_mixture) for z_e_x_mixture in z_e_mixtures]
 
-        sums[codes1, codes2, codes_mixture] += 1
+        codes = torch.stack([code.flatten() for code in codes], dim=0)
+        codes_mixtures = torch.stack([code.flatten() for code in codes_mixtures], dim=0)
+
+        for i in range(NUM_SOURCES):
+            if i == 0:
+                sums[i, codes_mixtures[i], torch.zeros_like(codes[i]), codes[i]] += 1
+            else:
+                sums[i, codes_mixtures[i], codes_mixtures[i-1], codes[i]] += 1
+        
+
         step += 1
     return step
 
@@ -134,7 +167,7 @@ def main(cfg):
     for param in model.parameters():
         param.requires_grad = False
 
-    sums = torch.zeros(cfg.num_codes, cfg.num_codes, cfg.num_codes).to(cfg.device)
+    sums = torch.zeros(NUM_SOURCES, cfg.num_codes, cfg.num_codes, cfg.num_codes).to(cfg.device)
 
     step = 0
     best_loss = -1.0
