6b96ea1528d5a915dd8e20b7215571292155c613
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2024-08-19 17:07:28 +0200

    sums_test.py is now working

--- lass_audio/lass/tests/sums_test.py
+++ lass_audio/lass/tests/sums_test.py
@@ -1,19 +1,6 @@
 """
 This file tests the correctness of the
-`lass_audio/lass/train_sums_graphical_model.py` sums computation. Since sparse
-tensors operations are not trivial and can lead to errors, the idea is to
-compute a smaller sums using a smaller dataset and a smaller K and compare it
-to its dense tensor counterpart. If they are equal, then the tensor in the
-sparse domain is computer correctly. This cannot be done for the real larger K,
-since the memory requirement is too large.
-
-TODO: there is a problem, since the VQVAE is trained with K=2048, it cannot be
-used with a different K, hence I should train another VQVAE just for this test,
-which I don't  think I will do. 
-
-TODO @1002: the solution is rather simple, just fake the data with a smaller
-K, create the mixture and see if the sparse matrix computed in the train_sums
-is equal to the dense matrix.
+`lass_audio/lass/train_sums_graphical_model.py` sums computation.
 """
 
 from tqdm import tqdm
@@ -46,7 +33,7 @@ def mock_compute_latent(x: torch.Tensor, k: int, flatten: bool = True):
         latents = latents.flatten().tolist()
     return latents
 
-def compute_sparse_sums(k: int, num_sources: int, iterations: int):
+def compute_sparse_sums(x: torch.Tensor, k: int, num_sources: int, iterations: int):
     """
     Compute the sparsesums of using num_sources arrays of length k.
     """
@@ -55,7 +42,6 @@ def compute_sparse_sums(k: int, num_sources: int, iterations: int):
     buffer_sums: List[List[int]] = [[] for _ in range(NUM_SOURCES-1)]
     batch_size = 8 
     device = torch.device("cpu")
-    x = torch.randn((batch_size, 524288, 1))
     xs = split_datapoints_by_step(x, batch_size, step=NUM_SOURCES, device=device)
     assert xs.shape == (num_sources, batch_size // num_sources, 524288, 1), f"xs wrong shape: {xs.shape}"
     mixtures = get_mixtures(datapoints=xs, device=device)
@@ -101,50 +87,65 @@ def compute_sparse_sums(k: int, num_sources: int, iterations: int):
     
     return sum_dist
 
-def compute_dense_sums(k: int, num_sources: int, iterations: int):
+def compute_dense_sums(x: torch.Tensor, k: int, num_sources: int, iterations: int):
     """
     Compute the dense sums of using num_sources arrays of length k.
     """
-    prefix_s, prefix_i, prefix_j, prefix_k, prefix_data = [], [], [], [], []
-    buffer_adds: List[List[int]] = [[] for _ in range(NUM_SOURCES)]
-    buffer_sums: List[List[int]] = [[] for _ in range(NUM_SOURCES-1)]
+    buffer_adds = []
+    buffer_sums = []
     batch_size = 8 
     device = torch.device("cpu")
-    x = torch.randn((batch_size, 524288, 1))
     xs = split_datapoints_by_step(x, batch_size, step=NUM_SOURCES, device=device)
     assert xs.shape == (num_sources, batch_size // num_sources, 524288, 1), f"xs wrong shape: {xs.shape}"
     mixtures = get_mixtures(datapoints=xs, device=device)
     assert mixtures.shape == (NUM_SOURCES-1, 2, 524288, 1), f"mixtures wrong shape: {mixtures.shape}"
     for i, x_i in enumerate(xs):
-        latent = mock_compute_latent(x_i, k, flatten=True)
-        assert len(latent) == ((batch_size // NUM_SOURCES) * k), f"x_i latent wrong shape: {torch.tensor(latent).shape}"
-        buffer_adds[i].append(latent)
+        latent = mock_compute_latent(x_i, k, flatten=False)
+        print(f"Dense latent {i}: {latent}, shape {latent.shape}")
+        # assert len(latent) == ((batch_size // NUM_SOURCES), k), f"x_i latent wrong shape: {torch.tensor(latent).shape}"
+        buffer_adds.append(latent)
     for i, m_i in enumerate(mixtures):
-        latent = mock_compute_latent(m_i, k, flatten=True)
-        assert len(latent) == ((batch_size // NUM_SOURCES) *k), f"m_i latent wrong shape: {torch.tensor(latent).shape}"
-        buffer_sums[i].append(latent)
-    sums_dist = torch.full((NUM_SOURCES-1, k, k, k), fill_value=0)
+        latent = mock_compute_latent(m_i, k, flatten=False)
+        print(f"Dense mixture latent {i}: {latent}, shape {latent.shape}")
+        # assert len(latent) == ((batch_size // NUM_SOURCES), k), f"m_i latent wrong shape: {torch.tensor(latent).shape}"
+        buffer_sums.append(latent)
+
+    codes = torch.stack(buffer_adds, dim=0)
+    codes_mixtures = torch.stack(buffer_sums, dim=0)
+    sums = torch.full((NUM_SOURCES-1, k, k, k), fill_value=0)
     for _ in tqdm(range(iterations), desc="Computing dense sums"):
         for i in range(NUM_SOURCES-1):
             if i == 0:
-                sums_dist[i, buffer_sums[i], buffer_adds[i], buffer_adds[i+1]] += 1
+                sums[i, codes_mixtures[i], codes[i], codes[i+1]] += 1
+                sums[i, codes_mixtures[i], codes[i+1], codes[i]] += 1
             else:
-                sums_dist[i, buffer_sums[i], buffer_sums[i-1], buffer_adds[i+1]] += 1
-    return sums_dist
+                sums[i, codes_mixtures[i], codes_mixtures[i-1], codes[i+1]] += 1
+                sums[i, codes_mixtures[i], codes[i+1], codes_mixtures[i-1]] += 1
+    return sums
 
+def get_coords(x: torch.Tensor):
+    non_zero_indices = torch.nonzero(x, as_tuple=True)
+    coords_dict = {tuple(id.item() for id in idx): x[idx].item() for idx in zip(*non_zero_indices)}
+    return coords_dict
+
+
+@test
 def test_sums():
     """
     Actually performs the test.
     """
-    K = 8
+    K = 12
     NUM_SOURCES = 3
-    ITERATIONS = 10_000
-    sparse_sums = compute_sparse_sums(k=K, num_sources=NUM_SOURCES, iterations=ITERATIONS)
-    dense_sums = compute_dense_sums(k=K, num_sources=NUM_SOURCES, iterations=ITERATIONS)
+    ITERATIONS = 1
+    batch_size = 8
+    x = torch.randn((batch_size, 524288, 1))
+    sparse_sums = compute_sparse_sums(x=x, k=K, num_sources=NUM_SOURCES, iterations=ITERATIONS)
+    sparse_sums = torch.from_numpy(sparse_sums.todense()).permute(0, 3, 1, 2)
+    dense_sums = compute_dense_sums(x=x, k=K, num_sources=NUM_SOURCES, iterations=ITERATIONS)
     
-    assert dense_sums == sparse_sums.todense(), f"""
-    Sparse sums: {sparse_sums.todense()}
-    Dense sums: {dense_sums}
+    assert torch.all(dense_sums == sparse_sums), f"""
+    Sparse sums: {get_coords(sparse_sums)}
+    Dense sums: {get_coords(dense_sums)}
     """
 
 
